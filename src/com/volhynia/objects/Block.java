faa8046a817baa2b47a8740a99abbde7296064cd
Jordan Gignac <gignjo16@gmail.com>
2019-08-04 12:54:40 -0400

    Remove unused imports

--- src/com/volhynia/objects/Block.java
+++ src/com/volhynia/objects/Block.java
@@ -1,6 +1,5 @@
 package com.volhynia.objects;
 
-//import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Rectangle;
 import java.util.LinkedList;
--- src/com/volhynia/objects/Player.java
+++ src/com/volhynia/objects/Player.java
@@ -1,6 +1,5 @@
 package com.volhynia.objects;
 
-import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Rectangle;
